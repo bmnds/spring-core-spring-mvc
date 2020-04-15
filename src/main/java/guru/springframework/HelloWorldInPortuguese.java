fbd69917d409076033c465817f5b0812578d15cb
Bruno Mendes <bruno.mendes@prodam.am.gov.br>
2020-04-14 21:20:02 -0400

    16. Hello, Profiles

--- src/main/java/guru/springframework/HelloWorldInPortuguese.java
+++ src/main/java/guru/springframework/HelloWorldInPortuguese.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!portuguese")
-public class HelloWorld implements HelloWorldService {
+@Profile("portuguese")
+public class HelloWorldInPortuguese implements HelloWorldService {
 	
 	public String sayHello(String to) { 
-		return "Hello, " + to;
+		return "Olá, " + to;
 	}
 
 }
