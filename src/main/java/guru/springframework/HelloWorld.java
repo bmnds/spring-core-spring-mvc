90b0f7377b0bdc1d47708a4af3b9fdff8b8136ed
Bruno Mendes <bruno.mendes@prodam.am.gov.br>
2020-04-14 21:24:41 -0400

    17. Hello, Default profile

--- src/main/java/guru/springframework/HelloWorld.java
+++ src/main/java/guru/springframework/HelloWorld.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!portuguese")
+@Profile("default")
 public class HelloWorld implements HelloWorldService {
 	
 	public String sayHello(String to) { 
